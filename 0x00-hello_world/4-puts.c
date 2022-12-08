ef4006a8b5f807ebdd68ddd4bd0d9457466acfae
chege-dan <danchege.w@outlook.com>
2022-12-08 13:18:03 -0800

    4 puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * author : Dan_Chege
+ * Description: main - Prints out a string to stdout.
+ *Return: 0 if success.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
