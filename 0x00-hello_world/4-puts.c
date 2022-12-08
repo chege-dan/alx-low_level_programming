6345eba69965bc1e766e43c19d8a93645e61811f
chege-dan <danchege.w@outlook.com>
2022-12-08 14:21:48 -0800

    commenting is hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* @main - Entry point
-* @brief: Prints out a string output using puts
-* Return: Always 0 (success)
+* Description: main - Prints out a string to stdout.
+* Return: 0 if success.
 */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* @function: main
-* @brief: Prints out a strin output using prrintf
-* @return: 0
+* Description: main - Prints out a string to stdout.
+* Return: 0 if success.
 */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
