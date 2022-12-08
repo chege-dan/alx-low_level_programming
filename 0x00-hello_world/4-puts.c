d25b11ce34e2b96d673655f28c94340c7e15b7e7
chegedan_ <86947604+chege-dan@users.noreply.github.com>
2022-12-09 01:23:10 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* @main - Entry point
-* @brief: Prints out a string output using puts
-* Return: Always 0 (success)
-*/
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
