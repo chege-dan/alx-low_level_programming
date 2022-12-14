0a42bbce7f01345d345fb9975e92cdc6efcd95a5
chegedan_ <danchege.w@outlook.com>
2022-12-15 01:15:59 +0300

    space

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-	do {
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-count++;
-_putchar('\n');
+do {
+    for (letter = 'a'; letter <= 'z'; letter++)
+        _putchar(letter);
+    count++;
+    _putchar('\n');
 } while (count < 10);
 }
