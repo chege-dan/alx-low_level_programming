1b20c21971a5c1c16cbd6d081aefef49dd608fc8
chegedan_ <danchege.w@outlook.com>
2022-12-15 08:59:01 +0300

    space

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@ int main(void)
 	int counter;
 	unsigned long num1, num2, sum;
 
-	num1 = 0;
-    num2 = 1;
-    for (counter = 0; counter < 50; counter++)
-	{
-        sum = num1 + num2;
-		printf("%lu", sum);
-        num1 = num2;
-        num2 = sum;
-		if (counter < 49)
-			printf(", ");
-		else
-			printf("\n");
-	}
-	return (0);
+num1 = 0;
+num2 = 1;
+for (counter = 0; counter < 50; counter++)
+{
+sum = num1 + num2;
+printf("%lu", sum);
+num1 = num2;
+num2 = sum;
+if (counter < 49)
+printf(", ");
+else
+printf("\n");
+}
+return (0);
 }
